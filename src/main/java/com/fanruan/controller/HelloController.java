4d353e9c0f552312a775fdde38317a5e528623e1
Yisen <13212152757@163.com>
2019-11-02 19:56:43 +0800

    springmvc day01

---
+++ src/main/java/com/fanruan/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.fanruan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//控制器的类
+@Controller
+public class HelloController {
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("Hello Springmvc");
+        return "success";
+    }
+}
